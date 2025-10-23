379db0914d77ddc31ee7db0439c76ea239635ced
abhishekagw <147487757+abhishekagw@users.noreply.github.com>
2025-10-23 15:22:44 +0530

    Update API base URL to production environment

--- client/src/lib/apiRequests.js
+++ client/src/lib/apiRequests.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const apiRequests = axios.create({baseURL:"http://localhost:8500/api",withCredentials:true,});
+const apiRequests = axios.create({baseURL:"https://real-estate-app-backend-qe8q.onrender.com/api",withCredentials:true,});
 
 export default apiRequests;
